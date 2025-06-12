0c3dcf9822e459dca9b919115ad834d81a2fd271
jujulcrane <jujulcrane@gmail.com>
2025-06-12 16:57:08 -0400

    add download resume to navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import ContactIcon from "$lib/assets/icons/ContactIcon.svelte";
 	import HomeIcon from "$lib/assets/icons/HomeIcon.svelte";
+  import DownloadIcon from "$lib/assets/icons/DownloadIcon.svelte";
 
-	// navbar-specific logic here
+	
+  // Function to handle resume download
+  function downloadResume(){
+    const link = document.createElement('a');
+    link.href = '/resume.pdf';
+    link.download = 'Juliet_Crane_Resume.pdf';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  }
 </script>
 
 <nav class="text-black p-4">
@@ -16,6 +26,14 @@
 			<li><a href="/music" class="hover:text-gray-300">Music</a></li>
 			<li><a href="/coming-soon..." class="hover:text-gray-300">Coming Soon</a></li>
       <li><a href="#Contact" class="hover:text-gray-300">Contact</a></li>
+      <li>
+        <button
+        on:click={downloadResume}
+        class="hover:text-gray-300 cursor-pointer"
+        aria-label="Download Juliet Crane's Resume">
+        <DownloadIcon size={20}/>
+      </button>
+    </li>
 		</ul>
 	</div>
 </nav>
