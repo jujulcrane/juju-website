8804e9896ae54ddc00a258b6c2e284ce4be59060
jujulcrane <jujulcrane@gmail.com>
2025-06-29 15:57:09 -0400

    add art and types

---
+++ src/lib/components/art/types/art.ts
@@ -0,0 +1,14 @@
+export interface ArtPiece {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  year: number;
+  medium?: string;
+}
+
+export interface ArtCategory {
+  name: string;
+  slug: string;
+  pieces: ArtPiece[];
+}
